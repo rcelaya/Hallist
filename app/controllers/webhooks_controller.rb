28ae4e3f54a30d37bf8009759ef7fb9c9c642382
waldix <waldix86@gmail.com>
2014-02-14 01:14:37 -0600

    Prueba para envio de mail, una vez que ya se pago el articulo

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,13 +1,11 @@
 class WebhooksController < ApplicationController
-  include Shopping::PaypalCheckoutHelper
-# En Rails 4 suprime el mensaje: "WARNING: Can't verify CSRF token authenticity" añadiendo:
   skip_before_filter
   protect_from_forgery :except => :receptor
+
   require 'json'
   require 'yajl/json_gem'
 
   def receptor
-    request.body.rewind  # in case someone already read it
     event_json = JSON.parse(request.body.read)
 
     item = event_json['data']['object']['reference_id']
@@ -22,7 +20,6 @@ class WebhooksController < ApplicationController
       @order.create_conekta_invoice(nil, @order.credited_total)
       Notifier.order_complete(@order, reference_id, description, amount, currency).deliver
     end
-    Notifier.order_complete(@order, reference_id, description, amount, currency).deliver
     msg = { :status => "200"}
     render json: msg
   end
