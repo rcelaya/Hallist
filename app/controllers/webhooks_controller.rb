176dff3a58d186eadab0c17fe50de54eb787fd5d
waldix <waldix86@gmail.com>
2014-02-14 01:56:24 -0600

    Prueba para envio de mail, una vez que ya se pago el articulo

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -8,15 +8,15 @@ class WebhooksController < ApplicationController
   def receptor
     event_json = JSON.parse(request.body.read)
 
-    item = event_json['data']['object']['reference_id']
-    reference_id = event_json['data']['object']['id']
-    description = event_json['data']['object']['description']
-    amount = event_json['data']['object']['amount'].to_i
-    amount = (amount/100)
-    currency = event_json['data']['object']['currency']
-    @order =  Order.find_by_number(item)
-
     if event_json['data']['object']['status'] == 'paid'
+      item = event_json['data']['object']['reference_id']
+      reference_id = event_json['data']['object']['id']
+      description = event_json['data']['object']['description']
+      amount = event_json['data']['object']['amount'].to_i
+      amount = (amount/100)
+      currency = event_json['data']['object']['currency']
+      @order =  Order.find_by_number(item)
+
       @order.create_conekta_invoice(nil, @order.credited_total)
       Notifier.order_complete(@order, reference_id, description, amount, currency).deliver
     end
