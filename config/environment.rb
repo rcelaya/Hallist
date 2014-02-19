714600a81cd48b90f9581d83ea9e025f72a9fd20
waldix <waldix86@gmail.com>
2014-02-19 03:16:41 -0600

    Se agrega modulo para revisar las ordenes que tiene elartista

---
+++ config/environment.rb
@@ -0,0 +1,25 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+require File.expand_path('../../lib/printing/invoice_printer', __FILE__)
+
+Paperclip.options[:command_path] = "/usr/local/bin"
+
+# Initialize the rails application
+Hadean::Application.initialize!
+Hadean::Application.configure do
+
+
+
+  config.after_initialize do
+    unless Settings.encryption_key
+      raise "
+      ############################################################################################
+      !  You need to setup the settings.yml
+      !  copy settings.yml.example to settings.yml
+      !
+      !  Make sure you personalize the passwords in this file and for security never check this file in.
+      ############################################################################################
+      "
+    end
+  end
+end
